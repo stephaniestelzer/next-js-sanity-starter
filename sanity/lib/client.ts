58edbdba26c0382c8c23807cac06d6549ad8a8de
Stephanie Stelzer <stephanie@stelzers.com>
2025-06-20 15:00:23 -0400

    initial commit

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,14 +1,10 @@
-import { createClient } from "next-sanity";
+import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn } from "../env";
+import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn,
-  perspective: "published",
-  stega: {
-    studioUrl: process.env.NEXT_PUBLIC_SITE_URL + "/studio",
-  },
-});
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+})
