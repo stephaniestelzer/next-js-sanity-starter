de3680fe106b3b4cb643f9d4448c630c0947ac17
Stephanie Stelzer <stephanie@stelzers.com>
2025-06-20 15:55:22 -0400

    studio set-up

---
+++ studio/schemaTypes/page.ts
@@ -0,0 +1,29 @@
+import {defineType, defineField} from 'sanity'
+
+export const page = defineType({
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [{ type: 'block' }],
+    }),
+  ],
+})
